2ea82d01fe242182aaa40603d2a6a504c441908b
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-18 17:27:34 +0000

    feat: add currency support + hard/soft load

--- crates/fpt_web/www/script.js
+++ crates/fpt_web/www/script.js
@@ -1,20 +1,77 @@
 window.onload = () => {
-    fill_table();
+    if (new URL(window.location).searchParams.has("hard_load")) {
+        hard_load();
+    } else {
+        soft_load();
+    }
 }
 
-function fill_table() {
-    let table = document.querySelector("main table tbody");
-    if (table === null) {
-        return Error("failed finding table");
+async function soft_load() {
+    console.time("soft load");
+
+    try {
+        await update_values();
+        await fill_table();
+    }
+    catch (error) {
+        console.error(error);
+    }
+    finally {
+        console.timeEnd("soft load");
+    }
+}
+
+async function hard_load() {
+    console.time("hard load");
+
+    try {
+        await update_assets();
+        await update_values();
+        await fill_table();
+    }
+    catch (error) {
+        console.error(error);
+    }
+    finally {
+        console.timeEnd("hard load");
+    }
+}
+
+async function update_assets() {
+    await fetch("http://localhost:5050/update_assets")
+        .catch((error) => {
+            throw error;
+        });
+}
+
+async function update_values() {
+    let currency = document.querySelector("#currency");
+    if (currency === null) {
+        throw Error("failed finding currency");
     }
 
-    fetch("http://localhost:5050/get_assets")
+    await fetch(`http://localhost:5050/update_values/${currency.value}`)
+        .catch((error) => {
+            throw error;
+        });
+}
+
+async function fill_table() {
+    let table = document.querySelector("main table tbody");
+    let currency = document.querySelector("#currency");
+    if (table === null || currency === null) {
+        throw Error("failed finding table || failed finding currency");
+    }
+
+    await fetch("http://localhost:5050/get_assets")
         .then((resp) => {
             return resp.json();
         }).then((data) => {
             if (typeof data != typeof []) {
-                return Error("unexpected json");
+                throw Error("unexpected json");
             }
+
+            table.innerHTML = "";
 
             let seen_categories = [];
             data.sort((a, b) => a.category < b.category);
@@ -25,11 +82,41 @@ function fill_table() {
                     seen_categories.push(asset.category);
                 }
 
+                let name = asset.name[0].toUpperCase() + asset.name.slice(1).toLowerCase();
+                let amount = undefined;
+                let code = asset.code;
+                let value = undefined;
+
+                switch (asset.category) {
+                    case "Cryptocurrency":
+                        amount = asset.amount / 100000000;
+                        break;
+                    default:
+                        amount = asset.amount;
+                        break;
+                }
+
+                switch (currency.value) {
+                    case "usd":
+                        value = asset.value.toLocaleString("en-US", { style: "currency", currency: "USD" });
+                        break;
+                    case "eur":
+                        value = asset.value.toLocaleString("de-DE", { style: "currency", currency: "EUR" });
+                        break;
+                    case "nok":
+                        value = asset.value.toLocaleString("no-NO", { style: "currency", currency: "NOK" });
+                        break;
+                    default:
+                        throw Error("unsupported currency");
+                }
+
                 table.innerHTML += `<tr>
-                    <td headers='name'>${asset.name}</td>
-                    <td headers='amount'>${asset.amount} ${asset.code}</td>
-                    <td headers='value'>${asset.value}</td>
+                    <td headers='name'>${name}</td>
+                    <td headers='amount'>${amount} ${code}</td>
+                    <td headers='value'>${value}</td>
                 </tr>`;
             });
+        }).catch((error) => {
+            throw error;
         });
 }
