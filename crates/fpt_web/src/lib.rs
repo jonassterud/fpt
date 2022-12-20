8b5c392f8a92625ba8b64a8569476e61cbffe0b7
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-20 22:25:35 +0000

    fix: enable logging for fpt_web

--- crates/fpt_web/src/lib.rs
+++ crates/fpt_web/src/lib.rs
@@ -2,14 +2,17 @@ mod paths;
 mod tests;
 
 use actix_cors::Cors;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
 use anyhow::Result;
 
 pub async fn start() -> Result<()> {
     HttpServer::new(|| {
         let cors = Cors::permissive();
 
-        App::new().wrap(cors).service(paths::www())
+        App::new()
+            .wrap(cors)
+            .wrap(Logger::default())
+            .service(paths::www())
     })
     .bind(("localhost", 8080))?
     .run()
