1a727e249150294fc88b36cdf036e901478cb19a
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-22 17:18:07 +0000

    feat: `get_assets`

--- crates/fpt_bindings/src/coinbase/mod.rs
+++ crates/fpt_bindings/src/coinbase/mod.rs
@@ -32,16 +32,40 @@ fn get_access_timestamp() -> Result<u64> {
 }
 
 /// Get Coinbase accounts and transform them into vector of [`Asset`].
-pub fn get_assets(config: &Config) -> Result<f64> {
+pub fn get_assets(config: &Config) -> Result<Vec<Asset>> {
+    let mut out = vec![];
+
     let key = get_access_key(config);
     let timestamp = get_access_timestamp()?;
-    let sign = get_access_sign(config, timestamp, "GET", "/api/v3/brokerage/accounts/", "")?;
+    let sign = get_access_sign(config, timestamp, "GET", "/api/v3/brokerage/accounts", "")?;
+
+    println!("{}\n{}", sign, timestamp);
 
     let url = "https://api.coinbase.com/api/v3/brokerage/accounts/";
     let resp = ureq::get(url)
         .set("Accept", "application/json; charset=utf-8")
+        .set("CB-ACCESS-KEY", key)
+        .set("CB-ACCESS-SIGN", &sign)
+        .set("CB-ACCESS-TIMESTAMP", &timestamp.to_string())
+        .set("User-Agent", "request")
         .call()?
         .into_json::<serde_json::Value>()?;
 
-    todo!();
+    for account in gv_vec("accounts", &resp)? {
+        let name = gv_str("name", account)?;
+        let available_balance = gv("available_balance", account)?;
+        let value = gv_str("balance", available_balance)?.parse::<f64>()?;
+        let currency = gv_str("currency", available_balance)?;
+
+        out.push(Asset {
+            category: AssetCategory::Currency,
+            name: name.to_string(),
+            code: currency.to_string(),
+            amount: value,
+            value_usd: None,
+            value_in_currency: None,
+        });
+    }
+
+    Ok(out)
 }
